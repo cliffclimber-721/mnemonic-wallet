9d35ae0291af4a71b67952eae6ebfd4084a072e3
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-02-09 17:52:44 +0900

    Added wallet.json for more information

--- routes/wallet/index.js
+++ routes/wallet/index.js
@@ -17,31 +17,35 @@ router.post('/newMnemonic', async(req,res) => {
 
 // TODO : 니모닉 코드와 패스워드를 이용해 keystore와 address를 생성합니다.
 router.post('/newWallet', async(req, res) => {
-    let password = req.body.password;
+    let password = req.body.password
     let mnemonic = req.body.mnemonic;
-
-    try{
-        lightwallet.keystore.createVault(
-            {
-                password: password,
-                seedPhrase: mnemonic,
-                hdPathString: "m/0' /0' /0'"
-            },
-            function(err, ks){
-                ks.keyFromPassword(password, function(err, pwDerivedKey) {
-                    ks.generateNewAddress(pwDerivedKey, 1);
-
-                    let address = (ks.getAddresses()).toString();
-                    let keystore = ks.serialize();
-
-                    res.json({keystore: keystore, address: address});
-                });
-            }
-        )
-    } catch(exception){
-        console.log("NewWallet ==>> " + exception);
+  
+    try {
+      lightwallet.keystore.createVault({
+        password: password, 
+        seedPhrase: mnemonic,
+        hdPathString: "m/0'/0'/0'"
+        },
+        function (err, ks) {
+          ks.keyFromPassword(password, function (err, pwDerivedKey) {
+            ks.generateNewAddress(pwDerivedKey, 1);
+  
+            let address = (ks.getAddresses()).toString();
+            let keystore = ks.serialize();
+  
+            fs.writeFile('wallet.json',keystore,function(err, data){
+              if(err) {
+                  res.json({code:999,message:"실패"});
+              } else {
+                  res.json({code:1,message:"성공"});
+              }
+            });
+          });
+        }
+      );
+    } catch (exception) { 
+      console.log("NewWallet ==>>>> " + exception);
     }
-
-});
+  });
 
 module.exports = router;
